5745ffd4853255913cfb278437d32b815b67f4ae
jasjamjos <jasperj@codev.com>
2024-10-23 13:55:31 +0800

    feat: add total-number-widget component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Tab, Tabs } from "@nextui-org/tabs";
 import { TotalNumberWidget } from "./components/TotalNumberWidget";
 import { ReferralTab } from "./tabs/ReferralsTab";
 import { RewardsTab } from "./tabs/RewardsTab";
+import { DollarSign, Gem, UsersRound } from "lucide-react";
 
 function App() {
   return (
     <>
-      <TotalNumberWidget />
-      <TotalNumberWidget />
-      <TotalNumberWidget />
+      <div className="flex space-x-4 m-5">
+        <TotalNumberWidget icon={<Gem size="3em" />} label="Total Points" value="999" />
+        <TotalNumberWidget icon={<DollarSign size="3em" />} label="Total Cash Incentives" value="999" />
+        <TotalNumberWidget icon={<UsersRound size="3em" />} label="Total Referrals" value="999" />
+        </div>
       <div className="flex w-full flex-col">
         <Tabs aria-label="Options">
           <Tab key="referrals" title="Referrals">
